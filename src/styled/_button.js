9a6dc5307d73a961ebad9e6d1f669577dd9360ac
Daniel Ram <danieldram@gmail.com>
2018-11-07 19:21:40 -0800

    add venzee button style

--- src/styled/_button.js
+++ src/styled/_button.js
@@ -1,6 +1,13 @@
 import styled from "styled-components"
 
 export const Button = styled.button`
-    padding: 10px;
-    background: #555;
+
+    padding: 16px 32px;
+    border: 1px #5a2d85;
+    border-radius: 2px;
+    background-color: #5a2d85;
+    box-shadow: -6px 6px 0 -3px #fff, -6px 6px 0 0 #5a2d85;
+    color: #fff;
+    line-height: 15px;
+
 `;
